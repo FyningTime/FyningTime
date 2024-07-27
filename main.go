50b948f1307ec2988ba2b467e7f0ed01d79f6b1a
Thraax Session <thraax.session@iatlas.technology>
2024-07-27 13:12:27 +0200

    Set up project

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }
